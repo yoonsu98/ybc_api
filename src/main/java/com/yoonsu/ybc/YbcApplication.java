f873cc9b919765ab182a9663e6ffe062519c219d
yoonsu98 <yoonsu98@kakao.com>
2023-12-14 22:38:19 +0900

    first init & db config

---
+++ src/main/java/com/yoonsu/ybc/YbcApplication.java
@@ -0,0 +1,13 @@
+package com.yoonsu.ybc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoonsu/ybc/YbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonsu.ybc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
