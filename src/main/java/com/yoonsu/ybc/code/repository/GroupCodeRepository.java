92d6d297ffe184784915bc05e7565f374c1c4ef1
y00nsh <yoonsu98@kakao.com>
2024-08-06 11:41:31 +0900

    계정 변경 테스트

--- src/main/java/com/yoonsu/ybc/code/repository/GroupCodeRepository.java
+++ src/main/java/com/yoonsu/ybc/code/repository/GroupCodeRepository.java
@@ -1,7 +1,5 @@
 package com.yoonsu.ybc.code.repository;
 
-import com.yoonsu.ybc.code.entity.Code;
-import com.yoonsu.ybc.code.entity.CodePK;
 import com.yoonsu.ybc.code.entity.GroupCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
